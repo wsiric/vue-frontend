25853bdbce31c5075cc9f91c11ab30aff8973464
wsiric <witsanu.sirichanyaphong@gmail.com>
2024-04-16 16:25:56 +0700

    togglePlay button.

--- src/components/PopulationChart.vue
+++ src/components/PopulationChart.vue
@@ -1,7 +1,5 @@
 <template>
-  <v-btn @click="test">
-  Button
-</v-btn>
+  <v-btn @click="togglePlay" :icon="isPlaying ? 'mdi-pause' : 'mdi-play'" size="large"></v-btn>
   <div>
     <ul>
       <li v-for="(value, key) in display" :key="key">
@@ -13,32 +11,47 @@
 
 <script setup>
 import axios from 'axios';
-import {onBeforeMount } from 'vue'
-import { ref} from '@vue/reactivity';
+import { onBeforeMount } from 'vue'
+import { ref } from '@vue/reactivity';
 
 const population = ref("")
 const years = ref("")
 const display = ref("")
+const isPlaying = ref(false)
+const currentIndex = ref(0)
 
 onBeforeMount(async () => {
   population.value = await axios.get('http://localhost:5000/population/all');
   years.value = await axios.get('http://localhost:5000/population/years');
-  await displayPopulation();
+
+  const firstYear = years.value.data[0]
+  display.value = {[firstYear] : population.value.data[firstYear]}
 })
 
-const test = () => {
-  console.log(display.value)
-}
-async function displayPopulation() {
-  for (const year of years.value.data) {
-    await displayPopulationHelper(year);
+const togglePlay = async () => {
+  isPlaying.value = !isPlaying.value;
+  if (isPlaying.value) {
+    await displayPopulation(currentIndex.value);
   }
 }
 
-async function displayPopulationHelper(year) {
-  display.value = { [year]: population.value.data[year] }
-  await delay(700);
+async function displayPopulation(startIndex) {
+  for (let i = startIndex; i < years.value.data.length; i++) {
+    currentIndex.value = i;
+    if (!isPlaying.value) {
+      break;
+    }
+    const year = years.value.data[i];
+    display.value = { [year]: population.value.data[year] };
+    await delay(100);
+  }
+  if (currentIndex.value === years.value.data.length - 1) {
+    isPlaying.value = false; 
+    currentIndex.value = 0;
+  }
 }
+
+
 
 async function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
