cdfa7d902b131192342a12d89134e28d5caa33a6
wsiric <witsanu.sirichanyaphong@gmail.com>
2024-04-16 20:25:59 +0700

    bar chart from chart.js

--- src/components/PopulationChart.vue
+++ src/components/PopulationChart.vue
@@ -1,6 +1,7 @@
 <template>
   <v-btn @click="togglePlay" :icon="isPlaying ? 'mdi-pause' : 'mdi-play'" size="large"></v-btn>
   <v-btn @click="resetButton" icon='mdi-cancel' size="large"></v-btn>
+  <v-btn @click="test" icon='mdi-cancel' size="large"></v-btn>
   <div>
     <ul>
       <li v-for="(value, key) in chartState" :key="key">
@@ -8,26 +9,65 @@
       </li>
     </ul>
   </div>
+  <v-container>
+    <div>
+      <canvas id="myChart"></canvas>
+    </div>
+  </v-container>
+
 </template>
 
 <script setup>
 import axios from 'axios';
-import { onBeforeMount } from 'vue'
+import { onBeforeMount, onMounted } from 'vue'
 import { ref } from '@vue/reactivity';
+import Chart from 'chart.js/auto';
+import { shallowRef } from 'vue';
 
+const labels = ref([])
+
+const chartData = {
+  labels: labels.value,
+  datasets: [{
+    label: "Population growth per country, 1950 to 2021",
+    borderColor: 'rgb(255,0 ,0)',
+    data: [],
+  }]
+}
+
+const config = {
+  type: 'bar',
+  data: chartData,
+  options: {
+    indexAxis: 'y',
+  }
+}
+
+const nTop = 10
 const population = ref("")
 const years = ref("")
 const chartState = ref("")
 const isPlaying = ref(false)
 const currentIndex = ref(0)
+const myChart = ref(null)
 
+onMounted(() => {
+  myChart.value = shallowRef(new Chart(
+    document.getElementById('myChart'),
+    config
+  ))
+})
 onBeforeMount(async () => {
   population.value = await axios.get('http://localhost:5000/population/all');
   years.value = await axios.get('http://localhost:5000/population/years');
 
-  initializeDisplayPopulation()
+  await initializeDisplayPopulation()
 })
 
+const test = () => {
+  changeChartData(["h",'a'], [0,1])
+  // myChart.value.update();
+}
 const togglePlay = async () => {
   isPlaying.value = !isPlaying.value;
   if (isPlaying.value) {
@@ -35,33 +75,52 @@ const togglePlay = async () => {
   }
 }
 
-const initializeDisplayPopulation = () => {
+const initializeDisplayPopulation = async () => {
   const firstYear = years.value.data[0]
-  chartState.value = {[firstYear] : population.value.data[firstYear]}
-} 
+  chartState.value = { [firstYear]: population.value.data[firstYear] }
+  const nTopCountryList = chartState.value[firstYear].slice(0, nTop);
+  const countryNames = nTopCountryList.map(item => item[0]);
+  const countryPopulation = nTopCountryList.map(item => item[1]);
+  changeChartData(countryNames, countryPopulation)
+}
 
-const resetButton = async() => {
-  isPlaying.value = false; 
+// bug that when user click reset button, its currentIndex get rewrite just after set to 0
+const resetButton = async () => {
+  isPlaying.value = false;
+  await delay(300)
   currentIndex.value = 0;
-  initializeDisplayPopulation()
+  console.log("resetButton : " + currentIndex.value)
+  initializeDisplayPopulation();
 }
 
 async function displayPopulation(startIndex) {
   for (let i = startIndex; i < years.value.data.length; i++) {
     currentIndex.value = i;
+    console.log("displayPop : " + currentIndex.value)
     if (!isPlaying.value) {
       break;
     }
     const year = years.value.data[i];
     chartState.value = { [year]: population.value.data[year] };
-    await delay(100);
+    await delay(250);
   }
   if (currentIndex.value === years.value.data.length - 1) {
-    isPlaying.value = false; 
+    isPlaying.value = false;
     currentIndex.value = 0;
   }
 }
 
+function changeChartData(label, newData) {
+  myChart.value.value.data.labels = label
+  myChart.value.value.data.datasets[0].data = newData
+  myChart.value.value.update();
+}
+
+function removeChartData() {
+  myChart.value.value.data.labels = []
+  myChart.value.value.data.datasets[0].data = []
+  myChart.value.value.update();
+}
 
 async function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
